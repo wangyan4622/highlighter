e2246c97cd3a747136989f29ea8c959f14e22652
wangyan <wangyan4622@gmail.com>
2013-10-15 23:02:42 +0800

    innerHTML

--- HighlighterProject/WebContent/js/highlighter.js
+++ HighlighterProject/WebContent/js/highlighter.js
@@ -1,5 +1,6 @@
 window.onload=function(){ 
   var a = document.getElementById("s");
   var b = document.getElementById("b");
-  
-}
+  b.innerHTML="<span>id</span>";
+  a.style.visibility="hidden";
+};
