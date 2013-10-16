a8637f210408217aab68918304f20e4fb6866f87
Yan Wang <wangyan4622@gmail.com>
2013-10-16 10:50:28 +0800

    commit trial from office

--- HighlighterProject/WebContent/js/highlighter.js
+++ HighlighterProject/WebContent/js/highlighter.js
@@ -1,6 +1,6 @@
 window.onload=function(){ 
   var a = document.getElementById("s");
   var b = document.getElementById("b");
-  b.innerHTML="<span>id</span>";
+  b.innerHTML="<span class='red'>aa</span>"
   a.style.visibility="hidden";
 };
