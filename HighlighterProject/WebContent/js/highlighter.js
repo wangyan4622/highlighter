c3b43dc94cbf365148a6e7146649abf9274f8a2d
Yan Wang <wangyan4622@gmail.com>
2013-10-16 18:14:02 +0800

    add parse logic

--- HighlighterProject/WebContent/js/highlighter.js
+++ HighlighterProject/WebContent/js/highlighter.js
@@ -1,6 +1,33 @@
-window.onload=function(){ 
-  var a = document.getElementById("s");
-  var b = document.getElementById("b");
-  b.innerHTML="<span class='red'>aa</span>"
-  a.style.visibility="hidden";
+window.onload = function() {
+	alert();
+	var a = document.getElementById("s");
+	var b = document.getElementById("b");
+	var content = a.innerHTML;
+	alert(content);
+	var hcontent = "";
+
+	// <
+	var lp = "&lt";
+	// >
+	var rp = "&gt;";
+
+	var begin, end, tagName, tagValue;
+//	while (content != null && content != "") {
+		begin = content.indexOf(lp);
+		if(begin>3){
+			tagValue=content.substr(0,begin);
+			hcontent+="<span class='xml-text'>"+tagValue+"</span>"
+			content=content.substr(begin);
+			continue;
+		}
+		end = content.indexOf(rp);
+		hcontent += "<span class='xml-punctuation'><</span>";
+		tagName = content.substr(begin + lp.length + 1, end - rp.length
+				- 1);
+		hcontent += "<span class='xml-tagname'>" + tagName + "</span>";
+		hcontent += "<span class='xml-punctuation'>></span>"
+		content = content.substr(end - rp.length, content.length);
+//	}
+
+	b.innerHTML = hcontent;
 };
