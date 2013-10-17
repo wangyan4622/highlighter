fe3f393a420d98fb9994a3bb9ab4157b35458eb4
Yan Wang <wangyan4622@gmail.com>
2013-10-17 17:42:28 +0800

    simple xml parse

--- HighlighterProject/WebContent/js/highlighter.js
+++ HighlighterProject/WebContent/js/highlighter.js
@@ -1,33 +1,35 @@
 window.onload = function() {
-	alert();
 	var a = document.getElementById("s");
 	var b = document.getElementById("b");
 	var content = a.innerHTML;
-	alert(content);
 	var hcontent = "";
-
 	// <
-	var lp = "&lt";
+	var lp = "&lt;";
 	// >
 	var rp = "&gt;";
 
-	var begin, end, tagName, tagValue;
-//	while (content != null && content != "") {
-		begin = content.indexOf(lp);
-		if(begin>3){
-			tagValue=content.substr(0,begin);
-			hcontent+="<span class='xml-text'>"+tagValue+"</span>"
-			content=content.substr(begin);
-			continue;
+	var begin, end, tagName, tagValue, hasSlash;
+	
+	while (content != "") {
+		begin = content.trim().indexOf(lp);
+		if (content.substr(begin + lp.length, 1) == '/') {
+			if (begin > 0) {
+				tagValue = content.substr(0, begin);
+				hcontent += "<span class='xml-text'>" + tagValue + "</span>"
+			}
+			hcontent += "<span class='xml-punctuation'>&lt;/</span>";
+			hasSlash = 1;
+		} else {
+			hcontent += "<span class='xml-punctuation'><</span>";
+			hasSlash = 0;
 		}
 		end = content.indexOf(rp);
-		hcontent += "<span class='xml-punctuation'><</span>";
-		tagName = content.substr(begin + lp.length + 1, end - rp.length
-				- 1);
+		tagName = content.substr(begin + lp.length + hasSlash,
+				end - (begin + lp.length + hasSlash)).trim();
 		hcontent += "<span class='xml-tagname'>" + tagName + "</span>";
-		hcontent += "<span class='xml-punctuation'>></span>"
-		content = content.substr(end - rp.length, content.length);
-//	}
+		hcontent += "<span class='xml-punctuation'>></span><br>"
+		content = content.substr(end + rp.length).trim();
+	}
 
 	b.innerHTML = hcontent;
 };
